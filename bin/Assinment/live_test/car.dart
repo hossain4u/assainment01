592ea184352c96865a46b110a405e24c17a2014c
hossain4u <sujonmazi20@gmail.com>
2024-09-28 00:19:01 +0600

    live test

--- bin/Assinment/live_test/car.dart
+++ bin/Assinment/live_test/car.dart
@@ -3,27 +3,10 @@ class Car {
   String model;
   int year;
 
-  Car({this.brand = '', this.model = '', this.year = 0}){
+  Car({this.brand = '', this.model = '', this.year = 0}){}
 
-  }
-
-
-  getBrand() {
-    return print('Brand Name: $brand');
-  }
-
-  getModel() {
-    return print('Model Name: $model');
-  }
-
-  getYear() {
-    return print('Year: $year');
-  }
-
-  void carAge() {
-    return print('Car Age:${DateTime
-        .now()
-        .year - year} years');
+   carAge() {
+    return DateTime.now().year - year;
   }
 }
 
--- bin/Assinment/live_test/main.dart
+++ bin/Assinment/live_test/main.dart
@@ -2,10 +2,10 @@ import 'car.dart';
 void main(){
 Car car=Car(brand:'Toyota',model: 'Corolla',year: 2015);
 
-car.getBrand();
-car.getModel();
-car.getYear();
-car.carAge();
-
+int age= car.carAge();
+print('Brand :'+car.brand);
+print('Model :'+car.model);
+print('year :'+car.year.toString());
+print('Car Age:$age years');
 
 }
